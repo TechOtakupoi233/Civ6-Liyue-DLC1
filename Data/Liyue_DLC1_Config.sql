59938ad83a0bd07b0156c64f8abcbef309d9e954
TechOtakupoi233 <slkj104@gmail.com>
2022-01-19 17:55:32 +0800

    Update Criteria
    Build up text and config for vanilla and XP2

---
+++ Data/Liyue_DLC1_Config.sql
@@ -0,0 +1,110 @@
+INSERT INTO PlayerItems (Domain, CivilizationType, LeaderType, Type, Icon, Name, Description, SortIndex)
+VALUES (
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_LIYUE_CL',
+	/* LdrType */ 'LEADER_KEQING',
+	/* ItmType */ 'UNIT_XINYAN_ROCK_BAND',
+	/* ItmIcon */ 'ICON_UNIT_XINYAN_ROCK_BAND',
+	/* ItmName */ 'LOC_UNIT_XINYAN_ROCK_BAND_NAME',
+	/* Dscrptn */ 'LOC_UNIT_XINYAN_ROCK_BAND_DESCRIPTION',
+	/* SortIdx */ 15
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_LIYUE_CL',
+	/* LdrType */ 'LEADER_KEQING',
+	/* ItmType */ 'BUILDING_WANGSHENG_FUNERAL_PARLOR',
+	/* ItmIcon */ 'ICON_BUILDING_WANGSHENG_FUNERAL_PARLOR',
+	/* ItmName */ 'LOC_BUILDING_WANGSHENG_FUNERAL_PARLOR_NAME',
+	/* Dscrptn */ 'LOC_BUILDING_WANGSHENG_FUNERAL_PARLOR_DESCRIPTION',
+	/* SortIdx */ 31
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_LIYUE_CL',
+	/* LdrType */ 'LEADER_KEQING',
+	/* ItmType */ 'BUILDING_WANMIN_RESTAURANT',
+	/* ItmIcon */ 'ICON_BUILDING_WANMIN_RESTAURANT',
+	/* ItmName */ 'LOC_BUILDING_WANMIN_RESTAURANT_NAME',
+	/* Dscrptn */ 'LOC_BUILDING_WANMIN_RESTAURANT_DESCRIPTION',
+	/* SortIdx */ 32
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_LIYUE_CL',
+	/* LdrType */ 'LEADER_KEQING',
+	/* ItmType */ 'BUILDING_HEYU_TEAHOUSE',
+	/* ItmIcon */ 'ICON_BUILDING_HEYU_TEAHOUSE',
+	/* ItmName */ 'LOC_BUILDING_HEYU_TEAHOUSE_NAME',
+	/* Dscrptn */ 'LOC_BUILDING_HEYU_TEAHOUSE_DESCRIPTION',
+	/* SortIdx */ 34
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_LIYUE_CL',
+	/* LdrType */ 'LEADER_KEQING',
+	/* ItmType */ 'BUILDING_WANWEN_BOOKHOUSE',
+	/* ItmIcon */ 'ICON_BUILDING_WANWEN_BOOKHOUSE',
+	/* ItmName */ 'LOC_BUILDING_WANWEN_BOOKHOUSE_NAME',
+	/* Dscrptn */ 'LOC_BUILDING_WANWEN_BOOKHOUSE_DESCRIPTION',
+	/* SortIdx */ 35
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_LIYUE_CL',
+	/* LdrType */ 'LEADER_KEQING',
+	/* ItmType */ 'BUILDING_BUBU_PHARMACY',
+	/* ItmIcon */ 'ICON_BUILDING_BUBU_PHARMACY',
+	/* ItmName */ 'LOC_BUILDING_BUBU_PHARMACY_NAME',
+	/* Dscrptn */ 'LOC_BUILDING_BUBU_PHARMACY_DESCRIPTION',
+	/* SortIdx */ 36
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_LIYUE_CL',
+	/* LdrType */ 'LEADER_NINGGUANG',
+	/* ItmType */ 'UNIT_XINYAN_ROCK_BAND',
+	/* ItmIcon */ 'ICON_UNIT_XINYAN_ROCK_BAND',
+	/* ItmName */ 'LOC_UNIT_XINYAN_ROCK_BAND_NAME',
+	/* Dscrptn */ 'LOC_UNIT_XINYAN_ROCK_BAND_DESCRIPTION',
+	/* SortIdx */ 15
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_LIYUE_CL',
+	/* LdrType */ 'LEADER_NINGGUANG',
+	/* ItmType */ 'BUILDING_WANGSHENG_FUNERAL_PARLOR',
+	/* ItmIcon */ 'ICON_BUILDING_WANGSHENG_FUNERAL_PARLOR',
+	/* ItmName */ 'LOC_BUILDING_WANGSHENG_FUNERAL_PARLOR_NAME',
+	/* Dscrptn */ 'LOC_BUILDING_WANGSHENG_FUNERAL_PARLOR_DESCRIPTION',
+	/* SortIdx */ 31
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_LIYUE_CL',
+	/* LdrType */ 'LEADER_NINGGUANG',
+	/* ItmType */ 'BUILDING_WANMIN_RESTAURANT',
+	/* ItmIcon */ 'ICON_BUILDING_WANMIN_RESTAURANT',
+	/* ItmName */ 'LOC_BUILDING_WANMIN_RESTAURANT_NAME',
+	/* Dscrptn */ 'LOC_BUILDING_WANMIN_RESTAURANT_DESCRIPTION',
+	/* SortIdx */ 32
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_LIYUE_CL',
+	/* LdrType */ 'LEADER_NINGGUANG',
+	/* ItmType */ 'BUILDING_HEYU_TEAHOUSE',
+	/* ItmIcon */ 'ICON_BUILDING_HEYU_TEAHOUSE',
+	/* ItmName */ 'LOC_BUILDING_HEYU_TEAHOUSE_NAME',
+	/* Dscrptn */ 'LOC_BUILDING_HEYU_TEAHOUSE_DESCRIPTION',
+	/* SortIdx */ 34
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_LIYUE_CL',
+	/* LdrType */ 'LEADER_NINGGUANG',
+	/* ItmType */ 'BUILDING_WANWEN_BOOKHOUSE',
+	/* ItmIcon */ 'ICON_BUILDING_WANWEN_BOOKHOUSE',
+	/* ItmName */ 'LOC_BUILDING_WANWEN_BOOKHOUSE_NAME',
+	/* Dscrptn */ 'LOC_BUILDING_WANWEN_BOOKHOUSE_DESCRIPTION',
+	/* SortIdx */ 35
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_LIYUE_CL',
+	/* LdrType */ 'LEADER_NINGGUANG',
+	/* ItmType */ 'BUILDING_BUBU_PHARMACY',
+	/* ItmIcon */ 'ICON_BUILDING_BUBU_PHARMACY',
+	/* ItmName */ 'LOC_BUILDING_BUBU_PHARMACY_NAME',
+	/* Dscrptn */ 'LOC_BUILDING_BUBU_PHARMACY_DESCRIPTION',
+	/* SortIdx */ 36
+);
---
+++ Data/Liyue_DLC1_Config_XP2.sql
@@ -0,0 +1,20 @@
+INSERT INTO PlayerItems (Domain, CivilizationType, LeaderType, Type, Icon, Name, Description, SortIndex)
+VALUES (
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_LIYUE_CL',
+	/* LdrType */ 'LEADER_KEQING',
+	/* ItmType */ 'UNIT_XINYAN_ROCK_BAND',
+	/* ItmIcon */ 'ICON_UNIT_XINYAN_ROCK_BAND',
+	/* ItmName */ 'LOC_UNIT_XINYAN_ROCK_BAND_NAME',
+	/* Dscrptn */ 'LOC_UNIT_XINYAN_ROCK_BAND_DESCRIPTION',
+	/* SortIdx */ 15
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_LIYUE_CL',
+	/* LdrType */ 'LEADER_NINGGUANG',
+	/* ItmType */ 'UNIT_XINYAN_ROCK_BAND',
+	/* ItmIcon */ 'ICON_UNIT_XINYAN_ROCK_BAND',
+	/* ItmName */ 'LOC_UNIT_XINYAN_ROCK_BAND_NAME',
+	/* Dscrptn */ 'LOC_UNIT_XINYAN_ROCK_BAND_DESCRIPTION',
+	/* SortIdx */ 15
+)
